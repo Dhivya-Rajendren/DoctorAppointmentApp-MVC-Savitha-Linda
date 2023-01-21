da91752c5e8a5b4dea8bfae3950c605be7e2ba18
Dhivya-Rajendren <rajendren.dhivya@gmail.com>
2023-01-21 12:14:41 +0530

    implementing the View models in

--- DoctorAppointmentApp-MVC/Controllers/AppointmentController.cs
+++ DoctorAppointmentApp-MVC/Controllers/AppointmentController.cs
@@ -1,5 +1,7 @@
 ﻿using DoctorAppointmentApp_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using DoctorAppointmentApp_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DoctorAppointmentApp_MVC.Controllers
 {
@@ -15,7 +17,48 @@ namespace DoctorAppointmentApp_MVC.Controllers
 
         public IActionResult MakeAppointment()
         {
-            return View();
+
+            List<Patient> patients = repo.GetPatients();
+            PatientDoctorViewModel viewModel = new PatientDoctorViewModel();
+            viewModel.Patients = new List<string>();
+            viewModel.PatientIds = new List<int>();
+            foreach (var item in patients)
+            {
+                viewModel.Patients.Add(item.PatientName);
+                viewModel.PatientIds.Add(item.PatientId);
+            }
+            List<Doctor> doctors = repo.GetDoctors();
+            viewModel.Doctors = new List<string>();
+            viewModel.DoctorIds = new List<int>();
+            foreach (var item in doctors)
+            {
+                viewModel.Doctors.Add(item.DoctorName);
+                viewModel.DoctorIds.Add(item.DoctorId);
+            }
+
+            List<SelectListItem> doctorItems = new List<SelectListItem>();
+
+            //Drop down - Value - taking the id of the doctor and Text -displaying the name
+
+            int i = 0;
+          while(i<doctors.Count)
+            {
+                doctorItems.Add(new SelectListItem(viewModel.Doctors[i], viewModel.DoctorIds[i].ToString()));
+                i++;
+            }
+
+            List<SelectListItem> patientItems = new List<SelectListItem>();
+            i = 0;
+            while(i<patients.Count)
+            {
+                patientItems.Add(new SelectListItem(viewModel.Patients[i], viewModel.PatientIds[i].ToString()));
+                i++;
+            }
+
+            ViewBag.DDLDoctors = doctorItems;
+            ViewBag.DDLPatients = patientItems;
+            
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ DoctorAppointmentApp-MVC/ViewModels/PatientDoctorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DoctorAppointmentApp_MVC.Models;
+
+namespace DoctorAppointmentApp_MVC.ViewModels
+{
+    public class PatientDoctorViewModel
+    {
+        public List<string> Patients { get; set; }
+
+        public List<string> Doctors { get; set; }
+
+        public Appointment Appointment { get; set; }
+
+        public List<int> PatientIds { get; set; }
+
+        public List<int> DoctorIds { get; set; }
+    }
+}
